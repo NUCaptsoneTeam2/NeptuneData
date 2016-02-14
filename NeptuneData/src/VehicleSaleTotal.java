dabc71cf47298d3af298b8f193dcd6f51f5d5048
Corey Spacht <coreyspacht@gmail.com>
2016-02-14 16:42:30 -0500

    Calculate total sales at the individual dealership level.

--- NeptuneData/src/VehicleSaleTotal.java
+++ NeptuneData/src/VehicleSaleTotal.java
@@ -89,9 +89,78 @@ public class VehicleSaleTotal {
     /**
      * Calculate the total car sales for a specified dealership.
      */
-    public static void calculateTotalCarSalesDealership() {
+    public static void calculateTotalCarSalesDealership(int dealershipID) {
+
+        // Import all records for selected employee.
+        // Declare the JDBC objects.
+        Connection conn;
+        Statement stmt;
+        ResultSet rs;
+
+        try {
+
+            // Generate SQL Query
+            String sql = "Select employeeID, "
+                    + "month, "
+                    + "modelID, "
+                    + "dealershipID, "
+                    + "promotionID, "
+                    + "totalSalesCount, "
+                    + "totalSalesAmount, "
+                    + "totalCost, "
+                    + "totalProfit "
+                    + "FROM VehicleSales "
+                    + "WHERE employeeID = %s";
+
+            conn = ConnectionFactory.getConnection();
+            stmt = conn.createStatement();
+
+            List<VehicleSale> salesList = new ArrayList<>();
+
+            rs = stmt.executeQuery(String.format(sql,dealershipID));
+
+            while (rs.next()) {
+                VehicleSale item = new VehicleSale(rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5),
+                        rs.getInt(6),
+                        rs.getInt(7),
+                        rs.getInt(8),
+                        rs.getInt(9));
+
+                salesList.add(item);
+            }
+
+            // Only calculate yearly sales total if records exist, otherwise notify the user
+            // no sales data exists for the specified employeeID.
+            if(salesList.size() > 0){
+
+                int totalSales = 0;
+
+                for(VehicleSale sale : salesList){
+
+                    totalSales =+ sale.getTotalSalesAmount();
+
+                }
+
+                NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
 
+                System.out.println(String.format("Total sales for dealership %s: %s", dealershipID, currencyFormat.format(totalSales)));
+
+            } else {
+
+                System.out.println("No sales data exists for the specified dealershipID: " + dealershipID);
+
+            }
+        }
+
+        // Handle errors.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
