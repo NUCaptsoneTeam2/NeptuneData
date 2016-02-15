408e9c348e1524578ae4332ef2eae3a574ca769f
Corey Spacht <coreyspacht@gmail.com>
2016-02-14 20:39:45 -0500

    Calculate total sales at the individual state level.

--- NeptuneData/src/VehicleSaleTotal.java
+++ NeptuneData/src/VehicleSaleTotal.java
@@ -168,6 +168,82 @@ public class VehicleSaleTotal {
      */
     public static void calculateTotalCarSalesState(String state) {
 
+        // Validate user has input a valid state abbreviation.
+
+
+
+        // Import all records for selected employee.
+        // Declare the JDBC objects.
+        Connection conn;
+        Statement stmt;
+        ResultSet rs;
+
+        try {
+
+            // Generate SQL Query
+            String sql = "Select a.employeeID, "
+                    + "a.month, "
+                    + "a.modelID, "
+                    + "a.dealershipID, "
+                    + "a.promotionID, "
+                    + "a.totalSalesCount, "
+                    + "a.totalSalesAmount, "
+                    + "a.totalCost, "
+                    + "a.totalProfit "
+                    + "FROM VehicleSales a "
+                    + "LEFT JOIN Dealerships b on a.dealershipID = b.dealershipID "
+                    + "WHERE b.State = \'%s\'";
+
+            conn = ConnectionFactory.getConnection();
+            stmt = conn.createStatement();
+
+            List<VehicleSale> salesList = new ArrayList<>();
+
+            rs = stmt.executeQuery(String.format(sql,state));
+
+            while (rs.next()) {
+                VehicleSale item = new VehicleSale(rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5),
+                        rs.getInt(6),
+                        rs.getInt(7),
+                        rs.getInt(8),
+                        rs.getInt(9));
+
+                salesList.add(item);
+            }
+
+            // Only calculate yearly sales total if records exist, otherwise notify the user
+            // no sales data exists for the specified employeeID.
+            if(salesList.size() > 0){
+
+                int totalSales = 0;
+
+                for(VehicleSale sale : salesList){
+
+                    totalSales =+ sale.getTotalSalesAmount();
+
+                }
+
+                NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+
+
+                System.out.println(String.format("Total sales for State %s: %s", state, currencyFormat.format(totalSales)));
+
+            } else {
+
+                System.out.println("No sales data exists for the specified State: " + state);
+
+            }
+        }
+
+        // Handle errors.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
