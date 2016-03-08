07dc714d97a16f31e565d6a31eb4a3b74ca20bfa
Corey Spacht <coreyspacht@gmail.com>
2016-03-08 13:20:01 -0500

    Added JavaDoc comments and added parameters to loadBaseData() method to specify which data elements should be imported and which should be ignored.

--- NeptuneData/src/Data.java
+++ NeptuneData/src/Data.java
@@ -1,56 +1,92 @@
-
+/**
+ * The Data class contains the method to load data from raw text files,
+ * parse the data based on data type, and persist the information to
+ * the production application database to await processing and incorporation
+ * into Neptune Auto metric calculations
+ *
+ * @version 1.0
+ */
 public class Data {
 
-	public static void loadBaseData() {
+	/**
+	 * Parse raw data files and persist the data into the application
+	 * database.
+	 * @param SalaryBand 	(required) Specify whether salary band information
+	 *                   	should be parsed and imported into the database.
+	 * @param Promotion		(required) Specify whether promotions data should
+	 *                  	be parsed and imported into the database.
+	 * @param Vehicle 		(required) Specify whether vehicle information should
+	 *                		be parsed and imported into the database.
+	 * @param Employee		(required) Specify whether employee information should
+	 *                      be parsed and imported into the database.
+	 * @param Dealership	(required) Specify whether dealership information should
+	 *                      be parsed and imported into the database.
+	 * @param VehicleSales	(required) Specify whether vehicle sales information should
+	 *                      be parsed and imported into the database.
+	 */
+	public static void loadBaseData(boolean SalaryBand,
+									boolean Promotion,
+									boolean Vehicle,
+									boolean Employee,
+									boolean Dealership,
+									boolean VehicleSales) {
 
 		// Load raw SalaryBand data
-		try {
-			ParseSalaryBand.loadRaw();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
+		if(SalaryBand){
+			try {
+				ParseSalaryBand.loadRaw();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
 		}
 		
 		// Load raw Promotion data
-		try {
-			ParsePromotion.loadRaw();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
+		if(Promotion) {
+			try {
+				ParsePromotion.loadRaw();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
 		}
-		
+
 		// Load raw Vehicle data
-		try {
-			ParseVehicle.loadRaw();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
+		if(Vehicle) {
+			try {
+				ParseVehicle.loadRaw();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
 		}
 
 		// Load raw Employee data
-		try {
-			ParseEmployee.loadRaw();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(Employee) {
+			try {
+				ParseEmployee.loadRaw();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
 		
 		// Load raw Dealership data
-		try {
-			ParseDealership.loadRaw();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(Dealership) {
+			try {
+				ParseDealership.loadRaw();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-
 		
-		//WARNING: Takes 20-25 minutes to complete. 
-
+		//WARNING: Takes 20-25 minutes to complete.
  		// Load raw VehicleSales data
-//		try {
-//			ParseVehicleSaleRaw.loadRaw();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}		
-//		
+		if(VehicleSales) {
+			try {
+				ParseVehicleSaleRaw.loadRaw();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
