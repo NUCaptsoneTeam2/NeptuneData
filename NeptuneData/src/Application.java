a6f5d5cf69bc4333800e08e723812291fd69c930
Corey Spacht <coreyspacht@gmail.com>
2016-03-08 20:07:13 -0500

    Fixed issue with national calculation by casting resulting SUM as bigint in SQL query.

--- NeptuneData/src/Application.java
+++ NeptuneData/src/Application.java
@@ -1,22 +1,25 @@
 
+/**
+ * Primary class to load and calculate Neptune Auto application data.
+ *
+ * @version 1.0
+ **/
 public class Application {
 
 	public static void main(String[] args) {
 
-		// Data
+		// Load data from text files.
 		try {
-			Data.loadBaseData();
+			Data.loadBaseData(true, true, true, true, true, true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		// Calculations
+		// Calculate pre-calculated fields.
 		try {
 			Calculate.run();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
-
-
+		}
 	}
 }
